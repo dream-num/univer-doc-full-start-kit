3aed91510a7a2cd7f9c16486eabd6fb31e7ba3af
hexf00 <hexf00@gmail.com>
2025-03-01 17:25:00 +0800

    feat: update to @univerjs/presets 0.6.2

--- src/setup-univer.ts
+++ src/setup-univer.ts
@@ -1,99 +1,46 @@
-import '@univerjs/design/lib/index.css'
-import '@univerjs/ui/lib/index.css'
-import '@univerjs/docs-hyper-link-ui/lib/index.css'
-import '@univerjs/drawing-ui/lib/index.css'
-import '@univerjs/docs-drawing-ui/lib/index.css'
-import '@univerjs-pro/collaboration-client/lib/index.css'
-import '@univerjs/thread-comment-ui/lib/index.css'
-import '@univerjs-pro/exchange-client/lib/index.css'
+import { LocaleType, UniverInstanceType, createUniver, defaultTheme, merge } from '@univerjs/presets'
 
-import { IConfigService, IUndoRedoService, LocaleType, LogLevel, Univer, UniverInstanceType } from '@univerjs/core'
-import { defaultTheme } from '@univerjs/design'
-import { UniverDocsPlugin } from '@univerjs/docs'
-import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
-import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
-import { UniverRenderEnginePlugin } from '@univerjs/engine-render'
-import { UniverUIPlugin } from '@univerjs/ui'
-import { UniverDocsHyperLinkPlugin } from '@univerjs/docs-hyper-link'
-import { UniverDocsHyperLinkUIPlugin } from '@univerjs/docs-hyper-link-ui'
-import { IImageIoService, UniverDrawingPlugin } from '@univerjs/drawing'
-import { UniverDrawingUIPlugin } from '@univerjs/drawing-ui'
-import { UniverDocsDrawingPlugin } from '@univerjs/docs-drawing'
-import { UniverDocsDrawingUIPlugin } from '@univerjs/docs-drawing-ui'
-import { UniverCollaborationPlugin } from '@univerjs-pro/collaboration'
-import { UniverDocsThreadCommentUIPlugin } from '@univerjs/docs-thread-comment-ui'
-import { UniverExchangeClientPlugin } from '@univerjs-pro/exchange-client'
-import { UniverDocsExchangeClientPlugin } from '@univerjs-pro/docs-exchange-client'
+import { UniverDocsCollaborationPreset } from '@univerjs/presets/preset-docs-collaboration'
+import docsCollaborationZhCN from '@univerjs/presets/preset-docs-collaboration/locales/zh-CN'
 
-import { COLLAB_SUBMIT_CHANGESET_URL_KEY, COLLAB_WEB_SOCKET_URL_KEY, SEND_CHANGESET_TIMEOUT_KEY, SNAPSHOT_SERVER_URL_KEY, UniverCollaborationClientPlugin } from '@univerjs-pro/collaboration-client'
+import { UniverDocsCorePreset } from '@univerjs/presets/preset-docs-core'
+import docsCoreZhCN from '@univerjs/presets/preset-docs-core/locales/zh-CN'
 
-import { FUniver } from '@univerjs/facade'
-import { locales } from './locale'
+import { UniverDocsDrawingPreset } from '@univerjs/presets/preset-docs-drawing'
+import docsDrawingZhCN from '@univerjs/presets/preset-docs-drawing/locales/zh-CN'
+
+import '@univerjs/presets/lib/styles/preset-docs-core.css'
+import '@univerjs/presets/lib/styles/preset-docs-collaboration.css'
+import '@univerjs/presets/lib/styles/preset-docs-drawing.css'
 
 export function setupUniver() {
-  const univer = new Univer({
+  const { univerAPI } = createUniver({
+    locale: LocaleType.ZH_CN,
+    locales: {
+      zhCN: merge(
+        {},
+        docsCoreZhCN,
+        docsCollaborationZhCN,
+        docsDrawingZhCN,
+      ),
+    },
     theme: defaultTheme,
-    locale: LocaleType.EN_US,
-    logLevel: LogLevel.VERBOSE,
-    locales,
-    override: [
-      [IUndoRedoService, null], // collaboration plugin will provide undo/redo service
+    collaboration: true,
+    presets: [
+      UniverDocsCorePreset({
+        container: 'univer',
+        collaboration: true,
+      }),
+      UniverDocsCollaborationPreset(),
+      UniverDocsDrawingPreset({ collaboration: true }),
     ],
   })
 
-  // core plugins
-  univer.registerPlugin(UniverRenderEnginePlugin)
-  univer.registerPlugin(UniverFormulaEnginePlugin)
-  univer.registerPlugin(UniverUIPlugin, {
-    container: 'univer',
-    footer: false,
-  })
-  univer.registerPlugin(UniverDocsPlugin)
-  univer.registerPlugin(UniverDocsUIPlugin, {
-    container: 'univerdoc',
-    layout: {
-      docContainerConfig: {
-        innerLeft: false,
-      },
-    },
-  })
-
-  // hyper link plugins
-  univer.registerPlugin(UniverDocsHyperLinkPlugin)
-  univer.registerPlugin(UniverDocsHyperLinkUIPlugin)
-
-  // drawing plugins
-  univer.registerPlugin(UniverDrawingPlugin, {
-    override: [[IImageIoService, null]], // DocsDrawingPlugin will provide image io service
-  })
-  univer.registerPlugin(UniverDrawingUIPlugin)
-  univer.registerPlugin(UniverDocsDrawingPlugin)
-  univer.registerPlugin(UniverDocsDrawingUIPlugin)
-  univer.registerPlugin(UniverDocsThreadCommentUIPlugin)
-
-  const configService = univer.__getInjector().get(IConfigService)
-  const host = window.location.host
-  configService.setConfig(SNAPSHOT_SERVER_URL_KEY, `/universer-api/snapshot`)
-  configService.setConfig(COLLAB_SUBMIT_CHANGESET_URL_KEY, `/universer-api/comb`)
-  configService.setConfig(COLLAB_WEB_SOCKET_URL_KEY, `ws://${host}/universer-api/comb/connect`)
-  configService.setConfig(SEND_CHANGESET_TIMEOUT_KEY, 200) // 200ms
-
-  univer.registerPlugin(UniverCollaborationPlugin)
-  univer.registerPlugin(UniverCollaborationClientPlugin, {
-    enableOfflineEditing: true,
-    enableSingleActiveInstanceLock: true,
-    // collaborationUniverTypes: [UniverInstanceType.UNIVER_DOC],
-    enableAuthServer: true,
-  })
-
-  univer.registerPlugin(UniverExchangeClientPlugin)
-  univer.registerPlugin(UniverDocsExchangeClientPlugin)
-
+  // check if the unit is already created
   const url = new URL(window.location.href)
   const unit = url.searchParams.get('unit')
-
   if (unit) {
-    // do nothing, wait for the unit to be loaded
+  // waiting for the unit to be loaded
   }
   else {
     fetch(`/universer-api/snapshot/${UniverInstanceType.UNIVER_DOC}/unit/-/create`, {
@@ -103,21 +50,21 @@ export function setupUniver() {
       },
       body: JSON.stringify({
         type: UniverInstanceType.UNIVER_DOC,
-        name: 'New Doc By Univer',
+        name: 'New Sheet By Univer',
         creator: 'user',
       }),
     })
+      .then((response) => {
+        if (!response.ok)
+          throw new Error('Failed to create new sheet')
 
-      .then((res) => {
-        if (!res.ok)
-          throw new Error('create unit failed')
-        return res.json()
+        return response.json()
       })
-      .then((res) => {
-        if (!res.unitID)
+      .then((data) => {
+        if (!data.unitID)
           throw new Error('create unit failed')
 
-        url.searchParams.set('unit', res.unitID)
+        url.searchParams.set('unit', data.unitID)
         url.searchParams.set('type', String(UniverInstanceType.UNIVER_DOC))
         window.location.href = url.toString()
       })
@@ -125,10 +72,6 @@ export function setupUniver() {
         console.error(error)
       })
   }
-
-  const univerAPI = FUniver.newAPI(univer)
-
-  // univerAPI.getActiveDocument()?.appendText('Hello World!')
 
   return univerAPI
 }
